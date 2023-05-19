a5f5bfa54de88c0cd95fdd482c2ef2e33a822c47
Chris Tran <87344458+ChrisTrannn@users.noreply.github.com>
2023-05-18 22:57:08 -0700

    implemented ranking/boolean retrieval

--- index.py
+++ index.py
@@ -7,8 +7,6 @@ import nltk
 from collections import defaultdict
 from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
-from nltk.corpus import stopwords
-nltk.download('stopwords')
 
 ''' 
 REPORT
@@ -25,6 +23,9 @@ REPORT
 invertedIndex = defaultdict(list)
 documentIDToURL = {}
 documentID = 0
+partList = list()
+fileNum = 1
+fileName = "offload" + str(fileNum) + ".txt"
 
 def writeToFile():
     global invertedIndex
@@ -51,7 +52,9 @@ def buildInvertedIndex(path):
     global invertedIndex
     global documentIDToURL
     global documentID
-    
+    global fileNum 
+    global fileName 
+    global partList
     # iterate over all the files in the zip folder
     with zipfile.ZipFile(path, 'r') as zipFile:
         for zipInfo in zipFile.infolist():
@@ -60,6 +63,18 @@ def buildInvertedIndex(path):
             if zipInfo.filename.endswith('.json'):
                 seenWords = set()
                 documentID += 1
+                
+                # If inverted index length is greater than 10000 we offload it
+                # and init a new clean dictionary to work on
+                """if len(invertedIndex) > 10000:
+                    currFile = open(fileName, "w")
+                    for word in sorted(invertedIndex.keys()):   
+                        currFile.write(word + ": " + str(invertedIndex[word]) + "\n")
+                    partList.append(fileName)
+                    invertedIndex.clear()
+                    currFile.close()
+                    fileNum += 1
+                    fileName = "offload" + str(fileNum) + ".txt"""
                 
                 # open the json file
                 with zipFile.open(zipInfo) as jsonFile:
@@ -80,11 +95,9 @@ def buildInvertedIndex(path):
                     text = soup.get_text()
                     # tokenize the text
                     words = re.findall(r"[a-zA-Z0-9]+", text.lower())
-                    # remove stop words
-                    wordSet = set(words) - set(stopwords.words("english"))
                     
                     # iterate over the words to build the inverted index
-                    for word in wordSet: 
+                    for word in words: 
                         if word in seenWords:
                             continue
                         seenWords.add(word)
@@ -95,21 +108,40 @@ def buildInvertedIndex(path):
                         invertedIndex[word].append((documentID, wordFrequency))
                         documentIDToURL[documentID] = data['url']
             
-            # write data to files    
+            # write data to files
             writeToFile()
+            
 
 def retrieve(terms):
-    terms = set(terms) - set(stopwords.words("english"))
-    
+    # computer science
+    validDocIDs = set()
+    tempDocIDs = set()
+  
+    # remove invalid search terms
+    for term in terms:
+        if term not in invertedIndex:
+            terms.remove(term)
+
+    # gets all the valid docIDs, using AND only
+    for term in terms:
+        for docID, termFreq in invertedIndex[term]:
+            tempDocIDs.add(docID)
+        if validDocIDs == set():
+            validDocIDs = tempDocIDs
+            tempDocIDs = set()
+            continue
+        validDocIDs = validDocIDs.intersection(tempDocIDs)
+        tempDocIDs = set()
+
     # find a match and calculate the sum of frequency for each page
     hit = {}
     for term in terms:
-        if term in invertedIndex:
-            for pageList in invertedIndex[term]:
-                if pageList[0] in hit:
-                    hit[pageList[0]] = hit[pageList[0]] + pageList[1]
+        for docID, termFreq in invertedIndex[term]:
+            if docID in validDocIDs:
+                if docID in hit:
+                    hit[docID] = hit[docID] + termFreq
                 else: 
-                    hit[pageList[0]] = pageList[1]
+                    hit[docID] = termFreq
     
     # sort in descending order based on frequency 
     hit = sorted(hit.items(), key=lambda item:item[1], reverse=True)
@@ -119,12 +151,11 @@ def retrieve(terms):
     while rank < 5 and rank < len(hit):
         print('Rank', rank + 1, ':', documentIDToURL[hit[rank][0]])
         rank += 1
-
+        
 if __name__ == '__main__':
-    path = 'analyst.zip'
+    path = "analyst.zip"
+    
     buildInvertedIndex(path)
-    #print(invertedIndex)
 
     searchTerm = input('Search Term: ').lower().strip().split(' ')
-    print(searchTerm)
     retrieve(searchTerm)
