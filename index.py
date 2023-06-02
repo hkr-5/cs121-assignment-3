7da6bd219260f05e3ad12532272493c0068aff63
Chris Tran <87344458+ChrisTrannn@users.noreply.github.com>
2023-06-01 20:56:22 -0700

    Update index.py

--- index.py
+++ index.py
@@ -213,65 +213,7 @@ def mergePartialIndexes():
     ff = open("tempFiles.txt", 'r')
     for i in ff.readlines():
         merge(mergeFile, i.strip())
-            
-''' FUNCTIONS FOR QUERYING '''
-def retrieve(terms):
-    # stem all the terms before querying
-    terms = [ps.stem(term) for term in terms]
-
-    validDocIDs = set()
-    tempDocIDs = set()
-
-    # remove invalid search terms
-    for term in terms:
-        if term not in invertedIndex:
-            terms.remove(term)
-
-    # gets all the valid docIDs, using AND only
-    for term in terms:
-        for docID, termFreq in invertedIndex[term]:
-            tempDocIDs.add(docID)
-        if validDocIDs == set():
-            validDocIDs = tempDocIDs
-            tempDocIDs = set()
-            continue
-        validDocIDs = validDocIDs.intersection(tempDocIDs)
-        tempDocIDs = set()
-
-    # find a match and calculate the sum of frequency for each page
-    hit = {}
-    for term in terms:
-        for docID, termFreq in invertedIndex[term]:
-            if docID in validDocIDs:
-                if docID in hit:
-                    hit[docID] = hit[docID] + termFreq
-                else: 
-                    hit[docID] = termFreq
-    
-    # sort in descending order based on frequency 
-    hit = sorted(hit.items(), key=lambda item:item[1], reverse=True)
-    
-    # print out top 5 hits
-    rank = 0
-    while rank < 5 and rank < len(hit):
-        print('Rank', rank + 1, ':', documentIDToURL[hit[rank][0]])
-        rank += 1
-
-def promptUser():
-    quit = False
-    print('-' * 10,  'Welcome', '-' * 10)
-    while (not quit):
-        searchTerm = input('Please Enter Search Term (enter \'*\' to quit): ')
-        if searchTerm == '*':
-            print("Terminating search session. See you next time!")
-            quit = True
-        else:
-            searchTerm = searchTerm.lower().strip()
-            if len(searchTerm) == 0:
-                print('Error: Please enter a valid term.')
-            else:
-                searchTerm = searchTerm.split(' ')
-                retrieve(searchTerm)
+    ff.close()
         
 if __name__ == '__main__':
     # building the inverted index
@@ -283,6 +225,3 @@ if __name__ == '__main__':
     
     # dump the data structures to disk
     dumpDataStructures()
-
-    # user input
-    promptUser()
